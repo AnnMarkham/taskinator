987cf9dd6322b1dcc85ca85da6a2d6fc604a363e
Ann Markham <ann.markham@outlook.com>
2020-10-11 11:50:13 -0700

    added functions to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+//--Form variables, querySelector and EventListener -- target entire form - not just button(use submit instead of click so that fucntion only runs at submit, not on every click.
+// on button element with type attribute with value submit, or press enter on keyboard = submit
 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit", createTaskHandler);
