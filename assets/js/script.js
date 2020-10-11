4973c172eeb87305c92483a7b1ecbbfcb01485ce
Ann Markham <ann.markham@outlook.com>
2020-10-11 13:05:47 -0700

    create task with comtent submitted through form - complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,26 @@ var createTaskHandler = function (event) {
 
   event.preventDefault();
 
+
+  // .value indicates the property in which the input is stored ---  figured out where this is by typing console.dir on the line after the var taskNameInput -to see where entered text went(note before added .class to end of var )
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give it a class name
+  taskInfoEl.className = "task-info";
+  //add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
